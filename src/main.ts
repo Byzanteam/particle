4f7bce700a4064e84bd08677ef2c33f2f4aa9dfd
moonRider <solar.pom@gmail.com>
2020-11-27 20:33:10 +0800

    feat: remove useless code

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,8 @@
-// auto register svelet:options.tag as component tag
 import MultipleLine from './fields/multiple-line.svelte'
 customElements.define('jet-multiple-line', MultipleLine)
-
-/**
- * // for custom elemnt name
- * import MultipleLine from './fields/multiple-line.svelte'
- * class CustomMultipleLine extends MultipleLineField {}
- * customElements.define('custom-multiple-line', CustomMultipleLine)
- *
- * document.body.innerHTML = `<custom-multiple-line field={"label": "hehe"} value="89"></custom-multiple-line>`
- */
-
 document.body.innerHTML = `
 	<jet-multiple-line field='{"label": "hehe"}' value="89"></jet-multiple-line>
 `
-
 document.querySelector('jet-multiple-line').addEventListener('change', (event: CustomEvent) => {
 	console.log('custom-change-event:', event.detail.value)
 }, false)
