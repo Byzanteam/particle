d2b2f5efd2bcc2db34bf3887624e7408d7ad1e11
moonRider <solar.pom@gmail.com>
2020-11-30 12:09:13 +0800

    feat: export MultipleLine field in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import MultipleLine from './fields/multiple-line.svelte'
-customElements.define('jet-multiple-line', MultipleLine)
-document.body.innerHTML = `
-	<jet-multiple-line field='{"label": "hehe"}' value="89"></jet-multiple-line>
-`
-document.querySelector('jet-multiple-line').addEventListener('change', (event: CustomEvent) => {
-	console.log('custom-change-event:', event.detail.value)
-}, false)
+
+export default {
+	MultipleLine,
+}
