6961dcf4023c6bfbd251cf53df502f93e05832b4
moonRider <solar.pom@gmail.com>
2020-12-02 12:54:16 +0800

    feat: add custom config to particle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
-import { config, transformConfig } from 'tailwind-config'
+const { config, transformConfig } = require('tailwind-config')
+
+const spacing = Array.from({ length: 21 }).reduce((acc, _, index) => {
+  acc[`${index}px`] = `${index}px`
+  return acc
+}, {})
 
 module.exports = {
-  ...transformConfig(config),
+  ...transformConfig({
+    ...config,
+    theme: {
+      ...config.theme,
+      spacing: {
+        ...config.spacing,
+        ...spacing,
+      },
+      lineHeight: {
+        24: '24px',
+      },
+    }
+  }),
   purge: {
     content: [
       "./src/**/*.svelte",
