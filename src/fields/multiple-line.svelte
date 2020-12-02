4758008a431d7b2af46444fb77ad8275d015de97
moonRider <solar.pom@gmail.com>
2020-12-02 12:54:56 +0800

    feat: finish multiple line field

--- src/fields/multiple-line.svelte
+++ src/fields/multiple-line.svelte
@@ -17,16 +17,16 @@ function updateValue(): void {
 $: _field = JSON.parse(field)
 </script>
 
-<div bind:this={root} class="text-2xl text-red">
-  <div class="field__label">{_field.label}</div>
+<div bind:this={root} class="mb-8px last:mb-0">
+  <div class="text-gray-2 text-body1">{_field.label}</div>
   {#if _field.description}
-    <div class="field__description">{_field.description}</div>
+    <div class="text-body2 text-gray-3">{_field.description}</div>
   {/if}
-  <div class="field__input-wrapper">
+  <div class="mt-9px mb-13px">
     <textarea
       bind:value
       on:input={updateValue}
-      class="resize-none"
+      class="resize-none w-full pt-4px pb-4px pl-8px pr-8px placeholder-gray-4 text-body1 text-gray-1 leading-24 border border-gray-7 rounded-lg focus:border-primary focus:outline-none focus:shadow-6"
       placeholder="请输入"
     />
   </div>
