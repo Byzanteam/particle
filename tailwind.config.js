f39745f3911c05768c7ab756de611a1980503073
moonRider <solar.pom@gmail.com>
2020-12-03 15:00:54 +0800

    feat: remove custom spacing config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,9 @@
 const plugin = require('tailwindcss/plugin')
 const { config, transformConfig } = require('tailwind-config')
 
-const spacing = Array.from({ length: 21 }).reduce((acc, _, index) => {
-  acc[index] = `${index}px`
-  return acc
-}, {})
-
 module.exports = {
   ...transformConfig({
     ...config,
-    theme: {
-      ...config.theme,
-      spacing: {
-        ...config.spacing,
-        ...spacing,
-      },
-      lineHeight: {
-        normal: '24px',
-      },
-    },
     corePlugins: {
       ...config.corePlugins,
       ringWidth: false,
@@ -35,6 +20,7 @@ module.exports = {
     enabled: true
   },
   plugins: [
+    ...(config.plugins || []),
     plugin(({ theme, addUtilities, variants }) => {
       const boxShadow = theme('boxShadow')
       const utilties = Object.entries(boxShadow).reduce((acc, [modifier, value]) => {
